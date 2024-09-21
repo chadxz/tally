24e29dda185ed52503fe8192d05b0af63f92890d
Chad McElligott <chad@smartrr.com>
2024-09-20 22:10:22 -0500

    Improve table schemas based on drizzle docs

--- apps/web/src/db/schema/tallies.ts
+++ apps/web/src/db/schema/tallies.ts
@@ -3,7 +3,10 @@ import { cuid2 } from 'drizzle-cuid2/postgres';
 
 export const tallies = pgTable("tallies", {
   id: cuid2("id").notNull().defaultRandom().primaryKey(),
-  item_id: cuid2("item_id").notNull(),
-  created_by: cuid2("created_by").notNull(),
-  created_at: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+  itemId: cuid2("item_id").notNull(),
+  createdBy: cuid2("created_by").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
 });
+
+export type InsertTally = typeof tallies.$inferInsert;
+export type SelectTally = typeof tallies.$inferSelect;
--- apps/web/src/db/schema/users.ts
+++ apps/web/src/db/schema/users.ts
@@ -1,10 +1,13 @@
 import { text, timestamp, pgTable } from "drizzle-orm/pg-core";
-import { cuid2 } from 'drizzle-cuid2/postgres';
+import { cuid2 } from "drizzle-cuid2/postgres";
 
 export const users = pgTable("users", {
   id: cuid2("id").notNull().defaultRandom().primaryKey(),
   name: text("name").notNull(),
-  created_by: cuid2("created_by").notNull(),
-  created_at: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
-  updated_at: timestamp("updated_at", { withTimezone: true }).notNull().defaultNow(),
+  createdBy: cuid2("created_by").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date())
 });
+
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
