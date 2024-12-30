1d746cb2dd55b75feb81f52a4b2cd1c25f978527
Chad McElligott <chad.mcelligott@gmail.com>
2024-12-29 22:05:25 -0600

    Update config.ts

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,6 +1,6 @@
 import { loadConfig } from "@tally/typedconfig";
 import { configSchema } from "../config/schema";
-import { join } from 'path';
+import { join } from 'node:path';
 
 const path = join(import.meta.dirname, '..', 'config')
 const typedConfig = loadConfig(path, configSchema);
