d60ccd392b512de7b8e33bb6e21ff4c55d046e12
Chad McElligott <chad.mcelligott@gmail.com>
2024-12-31 23:28:31 -0600

    Fix running drizzle-kit migrate

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,8 +1,10 @@
 import { loadConfig } from "@tally/typedconfig";
 import { configSchema } from "../config/schema";
-import { join } from 'node:path';
+import { resolve } from 'node:path';
 
-const path = join(import.meta.dirname, '..', 'config')
+// TODO: Use `import.meta.dirname` once drizzle-kit supports ESM
+// See https://github.com/drizzle-team/drizzle-kit/issues/107
+const path = resolve('.', 'config');
 const typedConfig = loadConfig(path, configSchema);
 
 export default typedConfig;
