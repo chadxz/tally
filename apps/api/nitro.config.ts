1f2a9cd3e65beb7eb006738435a92df04903137b
Chad McElligott <chad.mcelligott@gmail.com>
2025-01-15 19:55:32 -0600

    test out OpenAPI support of Nitro

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -1,6 +1,41 @@
+import { stripIndent } from "common-tags";
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
   compatibilityDate: '2025-01-11',
-  preset: "netlify"
+  preset: "netlify",
+  experimental: {
+    openAPI: true,
+  },
+  compressPublicAssets: true,
+  openAPI: {
+    production: "prerender",
+    meta: {
+      title: 'Tally API',
+      description: stripIndent`
+        Tally helps you collect ideas, count how many times they come up, then
+        use this data to make decisions. The Tally API gives you access to
+        operate on your tallies and related operational data.
+
+        ## Authentication
+
+        All requests to the Tally API must be authenticated using a Bearer
+        token. You can obtain a token by signing up for a free account at
+        https://tally.bot.
+
+        ## Credits
+
+        Made with ☕️ by [Chad McElligott](https://chadxz.dev).
+      `,
+    },
+    ui: {
+      swagger: false,
+      scalar: {
+        route: "/",
+        theme: "moon",
+        favicon: "favicon.ico",
+      }
+    }
+  }
 });
--- apps/api/server/routes/index.ts
+++
@@ -1,3 +0,0 @@
-export default eventHandler((event) => {
-  return "Hello Nitro.";
-});
