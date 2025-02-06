0d6ec97ca0d05088810cc0e900b7611977b74f40
Chad McElligott <chad.mcelligott@gmail.com>
2025-02-05 19:49:22 -0600

    Angry env

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -109,7 +109,7 @@ app.openAPIRegistry.register("Item", itemSchema);
 app.openapi(route, async (c) => {
   const { limit, cursor } = c.req.query();
   logger.info("Tally API config", { config });
-  logger.info("Environment for the app", { env: process.env, importMetaEnv: import.meta.env });
+  logger.info("Environment for the app", { env: { ...process.env }, importMetaEnv: import.meta.env });
   logger.info("Listing items", { limit, cursor });
   return c.json([], 200);
 });
